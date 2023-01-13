cf16131b5e3b2945cca59476cf3ee1b2c75f95d6
ElenaPre <33331@ukr.net>
2023-01-13 14:19:40 +0100

    add variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
-let computerMove = `kamień`;
-let playerMove ='papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+let playerMove = prompt("Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.");
